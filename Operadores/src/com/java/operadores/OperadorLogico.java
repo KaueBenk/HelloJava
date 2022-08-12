0d92f05a8548e5b3d1cb9f3cc14919c90d5f6d6b
Kauê Benk <kauebenk@gmail.com>
2022-08-12 15:31:05 -0300

    Operadores Lógicos (PT02)

--- Operadores/src/com/java/operadores/OperadorLogico.java
+++ Operadores/src/com/java/operadores/OperadorLogico.java
@@ -2,18 +2,33 @@ package com.java.operadores;
 
 public class OperadorLogico {
     public static void main(String[] args) {
+        // Exemplo 1 - E
         int idade = 75;
 
         boolean precisaVotar = idade >= 18 && idade < 70;
 
         System.out.println(precisaVotar);
 
-
-
+        // Exemplo 2 - Ou
         int nrAmarelo = 0, nrVermelho = 0;
 
         boolean suspenso = nrAmarelo == 2 || nrVermelho == 1;
 
         System.out.println(suspenso);
+
+        // Exemplo 3 - Ou exclusivo
+        int x = 11, y = 11;
+
+        boolean teste = x > 10 ^ y > 10;
+
+        System.out.println(teste);
+
+        // Exemplo 3 - Not
+        idade = 16;
+
+        boolean maioridade = ! (idade >= 18);
+
+        System.out.println(maioridade);
+
     }
 }
