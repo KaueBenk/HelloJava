fa7a2f8d608ad31bbfc7bd84448ff11c0cbb3408
Kauê Benk <kauebenk@gmail.com>
2022-08-12 15:14:49 -0300

    Operadores de Igualdade e Relacionais

---
+++ Operadores/src/com/java/operadores/OperadorIgualdadeRelacionais.java
@@ -0,0 +1,10 @@
+package com.java.operadores;
+
+public class OperadorIgualdadeRelacionais {
+    public static void main(String[] args) {
+        int idade = 18;
+        boolean maioridade = idade >= 18;
+
+        System.out.println(maioridade);
+    }
+}
