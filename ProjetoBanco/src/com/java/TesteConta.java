523c8059307f3e3827c2df923af51aa51a99f147
Kauê Benk <kauebenk@gmail.com>
2022-08-13 15:06:43 -0300

    Construtores

--- ProjetoBanco/src/com/java/TesteConta.java
+++ ProjetoBanco/src/com/java/TesteConta.java
@@ -2,28 +2,8 @@ package com.java;
 
 public class TesteConta {
     public static void main(String[] args) {
-        Conta contaCorrente = new Conta();
-        Conta contaPoupanca = new Conta();
-        Conta contaInvestimento = new Conta();
+        Conta cc = new Conta(50.0);
 
-        contaCorrente.numero = 11;
-        contaCorrente.saldo = 20.5;
-        contaCorrente.cliente.nome = "Kaue";
-        contaCorrente.cliente.idade = 18;
-
-        contaPoupanca.numero = 22;
-        contaPoupanca.saldo = 30;
-        contaPoupanca.cliente.nome = "Fulano";
-        contaPoupanca.cliente.idade = 40;
-
-        contaInvestimento.numero = 33;
-        contaInvestimento.saldo = 40;
-        contaInvestimento.cliente.nome = "Ciclano";
-        contaInvestimento.cliente.idade = 40;
-
-        System.out.println(contaCorrente.numero);
-        System.out.println(contaCorrente.saldo);
-        System.out.println(contaCorrente.cliente.nome);
-        System.out.println(contaCorrente.cliente.idade);
+        System.out.println(cc.recuperarSaldo());
     }
 }
